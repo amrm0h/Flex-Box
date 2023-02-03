624e4d06993df3a7fc52751bfab4f1611f3bcbc5
amr mohamed <amr.mahmoud@fixedmea.com>
2023-02-03 22:57:39 +0200

    solve responsive

--- src/ResponsiveNav/index.js
+++ src/ResponsiveNav/index.js
@@ -6,7 +6,7 @@ const ResponsiveNav = () => {
     <>
       <img src='/images/navResponsive.png' alt="" width="100%" />
       <hr />
-      <ul class="navigation">
+      <ul className="navigation">
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Products</a></li>
