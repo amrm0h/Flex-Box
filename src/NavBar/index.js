e69f47e812f3b8a2412e7e42b719fd8790d2a588
amr mohamed <amr.mahmoud@fixedmea.com>
2023-02-03 21:21:21 +0200

    add basic task ui of nav bar

---
+++ src/NavBar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './navBarStyles.scss';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Our Products</li>
+        <li>Contact Us</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar;
