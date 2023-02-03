5c23cda4eef53c8cc4e9ab8ef4026af10408cb23
amr mohamed <amr.mahmoud@fixedmea.com>
2023-02-03 22:00:14 +0200

    tags examle code basic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Rearrenged from './Rearrenge';
 
 function App() {
   return (
     <div className="App">
-      Test
+      <Rearrenged />
     </div>
   );
 }
