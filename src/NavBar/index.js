f062c4d20490a13ddd86295dc2f35fba5ecd78d1
amr mohamed <amr.mahmoud@fixedmea.com>
2023-02-03 21:26:41 +0200

    solve nav bar task

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -3,14 +3,26 @@ import './navBarStyles.scss';
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Our Products</li>
-        <li>Contact Us</li>
-      </ul>
-    </nav>
+    <>
+      <nav className='solved'>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Our Products</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+      &nbsp;
+      &nbsp;
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Our Products</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+    </>
   )
 }
 
