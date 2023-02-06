f09c3af09639735faff4fe20f4eefc42a04a05fa
amr mohamed <amr.mahmoud@fixedmea.com>
2023-02-06 11:11:47 +0200

    add create boxes comment and flex basis example

--- src/Boxes.js
+++ src/Boxes.js
@@ -2,6 +2,7 @@ const colors = [
   'black', '#8e8e8e', '#438b97', '#d90d0d', '#0b8927', '#d3966e', '#3e45e3', '#903ee3', '#0868ff', '#ff9308', '#3ee353', '#4e4e4e', 'black', '#8e8e8e', '#438b97', '#d90d0d', '#0b8927', '#d3966e', '#3e45e3', '#903ee3', '#0868ff', '#ff9308', '#3ee353', '#4e4e4e',
 ]
 
+// This Function to create random boxes (items) with predefined colors
 const createItems = (length = 12) => Array
   .from({ length }, (_, i) => i + 1)
   .map((num, i) => ({ name: num, bg: colors[i] || '#002ee4' }));
